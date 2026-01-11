738b977c89db0a5c5867629d0fddd2a830aec1c0
Seungmo Sohn <sohn_tm@naver.com>
2026-01-11 13:38:00 +0900

    docs: Add minishell documentation and enhance HTTP guide formatting

--- circle5/cpp_module/cpp_module_07/ex00/main.cpp
+++ circle5/cpp_module/cpp_module_07/ex00/main.cpp
@@ -8,8 +8,8 @@ int main() {
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+	std::string c = "charlie";
+	std::string d = "delta";
 	::swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
